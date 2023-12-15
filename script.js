97bde0394bfa02e47c11a0b81c82828cf7125467
AaaSsHhUu <anegi6723@gmail.com>
2023-12-15 14:27:11 +0530

    making responsive sidebar

---
+++ script.js
@@ -0,0 +1,10 @@
+const sidebar = document.getElementById("sidebar");
+const menuIcon = document.querySelector(".menu");
+const closeIcon = document.querySelector(".close");
+
+function showMenu(){
+    sidebar.style.left = '0';
+}
+function hideMenu(){
+    sidebar.style.left = "-340px";
+}
