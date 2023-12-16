039921f0b79ce2b0af9e9644e06a061626da8a89
AaaSsHhUu <anegi6723@gmail.com>
2023-12-16 21:32:26 +0530

    made sidebar responsive for phone screens

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const closeIcon = document.querySelector(".close");
 
 function showMenu(){
     sidebar.style.left = '0';
+    sidebar.style.width = "100%";
 }
 function hideMenu(){
+    sidebar.style.width = "330px";
     sidebar.style.left = "-350px";
 }
